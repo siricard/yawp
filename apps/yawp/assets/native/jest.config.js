0c6dbb89b9354fa00044e697d59ebcad6ed8b6fc
Simon Ricard <ricardsimon@hotmail.com>
2026-05-02 12:26:25 -0400

    test: stabilize RN Jest timeout

--- apps/yawp/assets/native/jest.config.js
+++ apps/yawp/assets/native/jest.config.js
@@ -11,4 +11,5 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation|@noble/ed25519|@noble/hashes|bs58|base-x|base64-js|react-native-keychain|nativewind|react-native-css-interop|react-native-reanimated)',
   ],
+  testTimeout: 20000,
 };
